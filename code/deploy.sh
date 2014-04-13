fff32d33a1ea8aed1479bae9dea8fa9dbbfbba27
Niklas von Oehsen <nv@oehsen.de>
2014-04-13 22:39:06 +0200

    update scripts

--- code/deploy.sh
+++ code/deploy.sh
@@ -1,10 +1,10 @@
 rsync -ave ssh /Volumes/Userdata/nik/bastel/2014-03-29_Pi_hacking/code pi@192.168.0.43:
 
 #python code/processvid_pi.py
-
-#ssh pi@192.168.0.43 -c "bash code/stop.sh"
-
-#ssh pi@192.168.0.43 -c "bash code/start.sh"
+sleep 1
+ssh pi@192.168.0.43 -c "bash code/stop.sh"
+sleep 1
+ssh pi@192.168.0.43 -c "bash code/start.sh"
 
 
 
--- code/stop.sh
+++ code/stop.sh
@@ -1,3 +1,3 @@
 PID=`ps -eaf | grep "python.*processvid*" | grep -v grep | sed -e "s/^pi\s*\([0-9]*\)\s.*$/\1/"`
 
-kill PID
+kill $PID
